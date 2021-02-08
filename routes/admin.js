8dc2d96475261fa673bfe78c712e9b1a3837b946
percevil <peeevil@gmail.com>
2021-02-08 14:10:11 -0800

    rendering more pug files

--- routes/admin.js
+++ routes/admin.js
@@ -10,8 +10,8 @@ const products = [];
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-});
+  res.render('add-product', { pageTitle: 'Add Product'});
+});// Used to be : sendFile(path.join(rootDir, 'views', 'add-product.html')
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
