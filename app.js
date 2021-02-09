09d1e4cdfbb2214d80f493aaeb8873531cdc7715
percevil <peeevil@gmail.com>
2021-02-08 16:14:31 -0800

    adding a layout to pugs

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404');
+    // Used to be here ☝︎.sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -10,8 +10,9 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
   const products = adminData.products;
   // ↑↑ This line takes the products in adminData.products and injects (passes) them into our template ⬇︎⬇︎ to use.
-  res.render('shop', { prods: products, docTitle: 'The Book Shop' });
+  res.render('shop', { prods: products, docTitle: 'The Book Shop', path: "/"});
   // ↑↑ This line renders the HTML from the file shop.pug. Then maps it to a js object {} key name (prods) and binds it to the variable (products). 
-});
+ 
+}); // 85. Path: (line#13) gives reference to line #17 in layouts/main-layouts so that thew js logic in that line will give the Shop link in the header the active class when that class is active.
 
 module.exports = router;
