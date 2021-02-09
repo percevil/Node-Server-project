81cd9d1d566bd6da2ce9ad5680e7a61034c06d00
percevil <peeevil@gmail.com>
2021-02-08 17:30:34 -0800

    adding dynamic content

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404');
+    res.status(404).render('404', {pageTitle: '404... Page Not Found'});
     // Used to be here ☝︎.sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
--- routes/admin.js
+++ routes/admin.js
@@ -11,8 +11,8 @@ const products = [];
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
   res.render('add-product', { pageTitle: 'Add Product', path: "/admin/add-product" });
-  // 
-});// Used to be : sendFile(path.join(rootDir, 'views', 'add-product.html')
+});
+// Used to be : sendFile(path.join(rootDir, 'views', 'add-product.html')
 //85. path: (line#13) gives reference to line #21 in layouts/main-layout so that the js logic in that line will give the Shop link in the header the active class when that path is active.
 
 // /admin/add-product => POST
